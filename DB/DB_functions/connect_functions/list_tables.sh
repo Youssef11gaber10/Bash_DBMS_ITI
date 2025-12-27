02b88a767036f6a151431b4c78736a31587df953
Nouran-Aly <nouran3ly@gmail.com>
2025-12-27 12:44:34 +0200

    Nouran: added list tables file

--- DB/DB_functions/connect_functions/list_tables.sh
+++ DB/DB_functions/connect_functions/list_tables.sh
@@ -1,5 +1,29 @@
+#!/use/bin/env bash
+
 function list_tables()
 {
- 
-         echo "List Tables function called" ;;
+    #check DB is selected?
+    if [[ -z "$DB_Dir/$Curr_DB" ]];then
+        echo -e "${RED}No database selected. please connect first.${RESET}"
+        return 1
+    fi
+
+    #check DB dir exists?
+    if [[ ! -d "$DB_Dir/$Curr_DB" ]];then 
+        echo -e "${RED}"$DB_Dir/$Curr_DB" Datase doesn't exist${RESET}"
+        return 1
+    fi
+
+    #check if the db deosnt have tables
+    if [[ ! -z "$(ls -A "$DB_Dir/$Curr_DB")" ]];then 
+        # list tables
+        echo -e "${GREEN}${BOLD}"$Curr_DB" Tables : ${RESET}"
+        ls "$DB_Dir/$Curr_DB"
+    else
+        echo -e "${RED}"$Curr_DB" Datase doesn't have tables yet${RESET}"
+        return 1
+    fi
+
+    
+
 }
